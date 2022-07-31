5f573937c199402a0099c93dbb721cb076558a4f
varshaagrawal5 <agrawal.varsha2009@gmail.com>
2022-07-31 15:25:07 -0400

    branch 6 commit

--- 1234.sh
+++ 1234.sh
@@ -1,3 +1,18 @@
 #! /bin/sh
 
-echo "test file inside develop"
+echo " I am learning devops. devops is fun, Linux is OS which is light, linux is light"> testing.txt
+
+maxcount = 0
+maxWord = ""
+
+for i in $@
+do 
+        sed -i 's/light/linux/' testing.txt
+        count ='grep -io $i testing.txt | wc -l'
+
+        if [ $count -gt $maxCount ]
+        then
+            maxCount = $count
+            maxWord = $i 
+        fi
+done
