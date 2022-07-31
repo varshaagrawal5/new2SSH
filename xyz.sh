ea29d23345bf492ae94ca0fbcbf0541cd57d591c
varshaagrawal5 <agrawal.varsha2009@gmail.com>
2022-07-31 10:17:56 -0400

    xyz commiting

---
+++ xyz.sh
@@ -0,0 +1,20 @@
+ #! /bin/sh
+
+   echo "i am learning devops, devops is fun, linux is OS which is light, linux is linux" > testing.txt
+
+   maxcount=0
+   maxword=""
+
+   for i in $@
+   do 
+           sed -i 's/light/linux/' testing.txt
+           count='grep -io $i testing.txt | wc -1' 
+
+           if [ $count -gt $maxcount ] 
+           then
+                     maxcount=$count
+                     maxword=$i
+            fi
+     done
+
+     echo "maximum times occured word: $maxword at $maxcount times"    
